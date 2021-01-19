32af0c264517dcbd50f3731e3187c09eb9f239d2
Nick Gray <nickgray87@gmail.com>
2021-01-19 16:47:36 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten()
